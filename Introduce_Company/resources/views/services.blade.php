ee2c4ae591b59c5fe6de6293b381113bf6b4d3fe
Easterfly7 <huynhhao792002@gmail.com>
2024-04-25 15:07:48 +0700

    25.4 part3

--- Introduce_Company/resources/views/services.blade.php
+++ Introduce_Company/resources/views/services.blade.php
@@ -1,220 +1,136 @@
 @extends('layout.layout')
 @section('body')
-{{-- <!--==============================Content=================================-->
-		<div class="content cont1"><div class="ic">More Website Templates @ TemplateMonster.com - February 24, 2014!</div>
-			<div class="container_12">
-				<div class="grid_12">
-					<h2 class="mb0">Our Services</h2>
-				</div>
-			</div>
-		</div>
-		<div class="gray_block gb1">
-			<div class="container_12">
-				<div class="grid_12">
-					<div class="responsive">
-						<ul class="a_content">
-<!-- 1 -->
-							<li>
-								<div class="card-front">
-									<div class="text2">Investment Solutions</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-								<div class="card-back">
-									<h2><a href="#">Click here</a></h2>
-								</div>
-								<!-- Content -->
-								<div class="all-content">
-									<div class="text2">Investment Solutions</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-							</li>
-							<li>
-								<div class="card-front">
-									<div class="text2">Outsourcing Programs</div>
-									<p>Demollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quisy</p>
-								</div>
-								<div class="card-back">
-									<h2><a href="#">Click here</a></h2>
-								</div>
-								<!-- Content -->
-								<div class="all-content">
-									<div class="text2">Outsourcing Programs</div>
-									<p>Demollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quisy</p>
-								</div>
-							</li>
-							<li>
-								<div class="card-front">
-									<div class="text2">Marketing Research</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-								<div class="card-back">
-									<h2><a href="#">Click here</a></h2>
-								</div>
-								<!-- Content -->
-								<div class="all-content">
-									<div class="text2">Marketing Research</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-							</li>
-<!-- 2 -->
-							<li>
-								<div class="card-front">
-									<div class="text2">Partnership Programs</div>
-									<p>Komollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, alesuada nibh</p>
-								</div>
-								<div class="card-back">
-									<h2><a href="#">Click here</a></h2>
-								</div>
-								<!-- Content -->
-								<div class="all-content">
-									<div class="text2">Partnership Programs</div>
-									<p>Komollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, alesuada nibh</p>
-								</div>
-							</li>
-							<li>
-								<div class="card-front">
-									<div class="text2">Money Saving Ideas</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-								<div class="card-back">
-									<h2><a href="#">Click here</a></h2>
-								</div>
-								<!-- Content -->
-								<div class="all-content">
-									<div class="text2">Money Saving Ideas</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-							</li>
-							<li>
-								<div class="card-front">
-									<div class="text2">Risk Management</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-								<div class="card-back">
-									<h2><a href="#">Click here</a></h2>
-								</div>
-								<!-- Content -->
-								<div class="all-content">
-									<div class="text2">Risk Management</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-							</li>
-<!-- 3 -->
-							<li>
-								<div class="card-front">
-									<div class="text2">Business Analysis</div>
-									<p>Olerat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuad</p>
-								</div>
-								<div class="card-back">
-									<h2><a href="#">Click here</a></h2>
-								</div>
-								<!-- Content -->
-								<div class="all-content">
-									<div class="text2">Business Analysis</div>
-									<p>Olerat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuad</p>
-								</div>
-							</li>
-							<li>
-								<div class="card-front">
-									<div class="text2">Insurance Services</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-								<div class="card-back">
-									<h2><a href="#">Click here</a></h2>
-								</div>
-								<!-- Content -->
-								<div class="all-content">
-									<div class="text2">Insurance Services</div>
-									<p>In mollis erat matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis</p>
-								</div>
-							</li>
-							<li class="last">
-								<div class="card-front">
-									<div class="text2">Expert Business Plans</div>
-									<p>Matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh</p>
-								</div>
-								<div class="card-back">
-									<h2><a href="#">Click here</a></h2>
-								</div>
-								<!-- Content -->
-								<div class="all-content">
-									<div class="text2">Expert Business Plans</div>
-									<p>Matt nequemer rutrum. Cras facilisis, nulla vel viverra auctor, leo magna sodales felis, quis malesuada nibh</p>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div> --}}
-        <div class="container_12">
-            <div class="grid_12 ">
-                <h2 class="mb0" style="
-                text-align: center;">Các dịch vụ</h2>
-            </div>
+
+<head>
+    <style>
+        url('https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap');
+
+body{
+
+  background-color: #eee;
+}
+
+.nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active {
+    color: #495057;
+    background-color: transparent;
+    border-color: transparent;
+    border-bottom: 3px solid #000;
+
+}
+
+.nav-tabs>li>a{
+
+  text-transform: uppercase;
+  color: #000;
+      font-weight: 500;
+}
+
+.nav-tabs .nav-link:hover {
+    border-color: transparent;
+    border-bottom: 3px solid #000
+}
+
+.navbar{
+
+      border-bottom: 1px solid #00000012;
+      padding-top: 0rem !important;
+      padding-bottom: 0rem !important;
+          background-color: #ffffff!important;
+}
+
+.navbar-brand{
+
+  font-size: 24px;
+  text-transform: uppercase;
+  font-family: 'Allerta Stencil', sans-serif;
+  font-weight: 500;
+}
+
+.nav-tabs {
+    border-bottom: none;
+    align-self: center;
+}
+
+.card{
+
+  border:none;
+
+}
+
+.card-body {
+    flex: 1 1 auto;
+    padding: 10px;
+}
+
+.card-text{
+
+  font-size: 13px;
+}
+
+.guarantee{
+
+  color: #05882c;
+  margin-left: 4px;
+  font-weight: 700
+
+}
+
+hr{
+    margin: 0.2rem 0;
+   color: #bfbebe;
+}
+
+.buttons button{
+
+  text-transform: uppercase;
+  font-size: 12px;
+  border-radius: 2px;
+}
+    </style>
+</head>
+
+
+
+        <div class="container-fluid px-0">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed">
+                <div class="container-fluid justify-content-center" >
+                    <ul class="nav nav-tabs" id="myTab" role="tablist" >
+                        <li class="nav-item" role="presentation"> <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Các dịch vụ</a> </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-    </div>
-    <div class="gray_block gb1">
-        <div class="container_12">
-            <div class="gallery">
-                <div class="grid_3">
-                    <a href="{{asset('img/1.jpg')}}" class="gal"><img src="{{asset('img/1.jpg')}}" alt=""></a>
-                    <div class="text1"><a href="#">Xây dựng nhà ở</a></div>
-                    Cung cấp dịch vụ xây dựng các căn nhà từ việc đào móng, xây dựng khung cấu trúc cho đến việc hoàn thiện nội thất và ngoại thất.
-                    <br>
-                    <a href="#" class="btn">Xem thêm...</a>
-                </div>
-                <div class="grid_3">
-                    <a href="{{asset('img/1.jpg')}}" class="gal bd1"><img src="{{asset('img/1.jpg')}}" alt=""></a>
-                    <div class="text1"><a href="#">Xây dựng công trình công nghiệp và thương mại</a></div>
-                    Bao gồm xây dựng các nhà máy, kho bãi, văn phòng, cửa hàng, và các cơ sở thương mại khác.
-                    <br>
-                    <a href="#" class="btn">Xem thêm...</a>
-                </div>
-                <div class="grid_3">
-                    <a href="{{asset('img/1.jpg')}}" class="gal bd2"><img src="{{asset('img/1.jpg')}}" alt=""></a>
-                    <div class="text1"><a href="#">Sửa chữa và nâng cấp</a></div>
-                    Sửa chữa và nâng cấp các công trình đã tồn tại, bao gồm việc cải thiện cấu trúc, nâng cấp hệ thống điện, nước, và nội thất.
-                    <br>
-                    <a href="#" class="btn">Xem thêm...</a>
-                </div>
-                <div class="grid_3">
-                    <a href="{{asset('img/1.jpg')}}" class="gal bd3"><img src="{{asset('img/1.jpg')}}" alt=""></a>
-                    <div class="text1"><a href="#">Thiết kế kiến trúc</a></div>
-                    Thiết kế kiến trúc, tạo ra các kế hoạch và ý tưởng cho dự án xây dựng.
-                    <br>
-                    <a href="#" class="btn">Xem thêm...</a>
-                </div>
-                <div class="clear"></div>
-                <div class="grid_3">
-                    <a href="{{asset('img/1.jpg')}}" class="gal"><img src="{{asset('img/1.jpg')}}" alt=""></a>
-                    <div class="text1"><a href="#">Quản lý dự án</a></div>
-                    Quản lý dự án để đảm bảo rằng mọi khía cạnh của dự án được thực hiện một cách hiệu quả và đúng hẹn.
-                    <br>
-                    <a href="#" class="btn">Xem thêm...</a>
-                </div>
-                <div class="grid_3">
-                    <a href="{{asset('img/1.jpg')}}" class="gal bd1"><img src="{{asset('img/1.jpg')}}" alt=""></a>
-                    <div class="text1"><a href="#">Dịch vụ tư vấn và giám sát</a></div>
-                    Bao gồm các dịch vụ tư vấn về kỹ thuật, giải pháp thiết kế, và giám sát thi công để đảm bảo chất lượng và tuân thủ các quy định kỹ thuật và an toàn.
-                    <br>
-                    <a href="#" class="btn">Xem thêm...</a>
-                </div>
-                <div class="grid_3">
-                    <a href="{{asset('img/1.jpg')}}" class="gal bd2"><img src="{{asset('img/1.jpg')}}" alt=""></a>
-                    <div class="text1"><a href="#">Xử lý chất thải và bảo vệ môi trường</a></div>
-                    xử lý chất thải và thực hiện các biện pháp bảo vệ môi trường trong quá trình xây dựng.
-                    <br>
-                    <a href="#" class="btn">Xem thêm...</a>
-                </div>
-                <div class="grid_3">
-                    <a href="{{asset('img/1.jpg')}}" class="gal bd3"><img src="{{asset('img/1.jpg')}}" alt=""></a>
-                    <div class="text1"><a href="#">Xây dựng cơ sở hạ tầng</a></div>
-                    Bao gồm xây dựng các hạ tầng như đường, cống, cầu, và các công trình công cộng khác.
-                    <br>
-                    <a href="#" class="btn">Xem thêm...</a>
+        <div class="container mt-2 mb-5">
+            <div class="products">
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        </div>
+                        <div class="row g-3">
+                            @foreach ($pr1 as $p1 )
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <a href="{{route('services.show',$p1->id)}}"> <img src="{{ asset('img/1.jpg')}}" class="card-img-top"></a>
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between"> <span class="font-weight-bold">{{$p1->name}}</span>
+                                        {{-- <span class="font-weight-bold">$550</span> --}}
+                                    </div>
+
+                                        <p class="card-text mb-1 mt-1">{{$p1->description}}</p>
+                                    </div>
+                                    <hr>
+                                    {{-- <div class="card-body">
+                                        <div class="text-right buttons"> <button class="btn btn-outline-dark">add to wishlist</button> <button class="btn btn-dark">Add to cart</button> </div>
+                                    </div> --}}
+                                </div>
+                            </div>
+                            @endforeach
+
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 @endsection
+
+
--- Introduce_Company/routes/web.php
+++ Introduce_Company/routes/web.php
@@ -4,21 +4,21 @@ use App\Models\Product_Type;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductTypeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ServiceController;
 Route::get('/', function () {
     return view('index');
 })->name('home');
 Route::get('/about', function () {
     return view('about');
 })->name('about');
-Route::get('/services', function () {
-    return view('services');
-})->name('services');
-Route::get('/projects', function () {
-    return view('projects');
-})->name('projects');
+
 Route::get('/contacts', function () {
     return view('contacts');
 })->name('contacts');
+Route::get('/services', [ServiceController::class, 'index'])->name('services');
+Route::get('/services/{id?}', [ServiceController::class, 'show'])->name('services.show');
+Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
 Route::get('/product', [ProductTypeController::class, 'index'])->name('product');
 Route::get('/list-product/{id?}', [ProductController::class, 'list'])->name('list');
 Route::get('/show-product/{id?}', [ProductController::class, 'show'])->name('show');
